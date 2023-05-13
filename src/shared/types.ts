b8cd3209ed2eb95829b976940da826d47d3fe2f9
Nurzhan Bizhan <89019371+NurikBK@users.noreply.github.com>
2023-05-14 02:46:37 +0600

    finish project

---
+++ src/shared/types.ts
@@ -0,0 +1,13 @@
+export type SelectedPage = "home" | "benefits" | "ourclasses" | "contactus";
+
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
